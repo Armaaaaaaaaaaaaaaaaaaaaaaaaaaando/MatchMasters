89588fe265e7c51e6c23deb5f283df8615d07691
Armando <111616936+Armaaaaaaaaaaaaaaaaaaaaaaaaaaando@users.noreply.github.com>
2023-10-17 14:40:59 -0300

    Criação de pastas

--- src/Usuarios/Pessoa.java
+++ src/Usuarios/Pessoa.java
@@ -1,4 +1,22 @@
 package src.Usuarios;
 
-public class Pessoa {
+public abstract class Pessoa {
+    private String nome;
+    private Integer idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
 }
