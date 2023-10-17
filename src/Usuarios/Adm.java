7e0668b944a28a29491268cf98604a46e474557c
Armando <111616936+Armaaaaaaaaaaaaaaaaaaaaaaaaaaando@users.noreply.github.com>
2023-10-17 14:51:04 -0300

    Criação de pastas

---
+++ src/Usuarios/Adm.java
@@ -0,0 +1,23 @@
+package src.Usuarios;
+
+public class Adm extends Pessoa{
+    private String login;
+
+    private String senha;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
---
+++ src/Usuarios/Jogador.java
@@ -0,0 +1,50 @@
+package src.Usuarios;
+
+public class Jogador extends Pessoa{
+    //o clube é do tipo clube, está string até q a classe clube seja criada
+    private String Clube;
+    private String Posicao;
+    private String Data_admisão;
+    private String Data_final_contrato;
+    private Integer Temporadas;
+
+    public String getClube() {
+        return Clube;
+    }
+
+    public void setClube(String clube) {
+        Clube = clube;
+    }
+
+    public String getPosicao() {
+        return Posicao;
+    }
+
+    public void setPosicao(String posicao) {
+        Posicao = posicao;
+    }
+
+    public String getData_admisão() {
+        return Data_admisão;
+    }
+
+    public void setData_admisão(String data_admisão) {
+        Data_admisão = data_admisão;
+    }
+
+    public String getData_final_contrato() {
+        return Data_final_contrato;
+    }
+
+    public void setData_final_contrato(String data_final_contrato) {
+        Data_final_contrato = data_final_contrato;
+    }
+
+    public Integer getTemporadas() {
+        return Temporadas;
+    }
+
+    public void setTemporadas(Integer temporadas) {
+        Temporadas = temporadas;
+    }
+}
