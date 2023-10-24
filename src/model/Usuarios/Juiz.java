b2b5434b43b34205b05d07b45e1b834be20c9090
Armando <111616936+Armaaaaaaaaaaaaaaaaaaaaaaaaaaando@users.noreply.github.com>
2023-10-24 13:43:12 -0300

    Criando construtor do Juiz

--- src/model/Usuarios/Juiz.java
+++ src/model/Usuarios/Juiz.java
@@ -3,6 +3,11 @@ package src.model.Usuarios;
 public class Juiz extends Pessoa{
     private Integer Partidas_apitadas;
 
+    public Juiz(String nome, Integer idade, Integer partidas_apitadas){
+        this.setNome(nome);
+        this.setIdade(idade);
+        this.Partidas_apitadas = partidas_apitadas;
+    }
     public Integer getPartidas_apitadas() {
         return Partidas_apitadas;
     }
