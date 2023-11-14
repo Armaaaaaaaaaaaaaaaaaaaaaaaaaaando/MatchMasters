bc1ca225c7f5c8ef8e83cf3cda0b5c024eabb81e
Armando <111616936+Armaaaaaaaaaaaaaaaaaaaaaaaaaaando@users.noreply.github.com>
2023-11-14 10:25:15 -0300

    Atualização: Criação das exceções de JogadorDao, e interface jogador dao

--- src/dao/Usuarios/IJogadorDao.java
+++ src/dao/Usuarios/IJogadorDao.java
@@ -1,11 +1,12 @@
 package src.dao.Usuarios;
 
+import src.Excecoes.Usuarios.JogadorException;
 import src.model.Jogo.Clube;
 import src.model.Usuarios.Jogador;
 
 public interface IJogadorDao {
-    public void save(Clube c, Jogador a);
-    public void delete(Clube c,Jogador a);
-    public void update(Clube c,Jogador admvelho, Jogador Admnovo);
-    public Jogador find(Clube c,Jogador a);
+    public void save(Clube c, Jogador a) throws JogadorException;
+    public void delete(Clube c,Jogador a) throws JogadorException;
+    public void update(Clube c,Jogador admvelho, Jogador Admnovo) throws JogadorException;
+    public Jogador find(Clube c,Jogador a) throws JogadorException;
 }
--- src/dao/Usuarios/JogadorDao.java
+++ src/dao/Usuarios/JogadorDao.java
@@ -1,10 +1,13 @@
 package src.dao.Usuarios;
 
+import src.Excecoes.Usuarios.JogadorException;
 import src.model.Jogo.Clube;
 import src.model.Usuarios.Jogador;
 
 import java.util.HashMap;
 import java.util.LinkedList;
+
+import static src.util.Constantes.*;
 
 public class JogadorDao implements IJogadorDao{
     private Clube c;
@@ -12,47 +15,65 @@ public class JogadorDao implements IJogadorDao{
     private HashMap<String, LinkedList> lista = new HashMap<>();
 
     @Override
-    public void save(Clube clube, Jogador a) {
+    public void save(Clube clube, Jogador a) throws JogadorException {
         if(lista.containsKey(clube.getNome())){
             if(!lista.values().contains(a)){
                 jogadores.add(a);
                 lista.put(clube.getNome(),jogadores);
             }
+            else{
+                throw new JogadorException(SaveError, null);
+            }
+        }
+        else{
+            throw new JogadorException(SaveError, null);
         }
     }
 
     @Override
-    public void delete(Clube c,Jogador a) {
+    public void delete(Clube c,Jogador a) throws JogadorException{
         if(lista.containsKey(c.getNome())){
             if(lista.values().contains(a)){
                 lista.values().remove(a);
             }
+            else{
+                throw new JogadorException(DeleteError, null);
+            }
+        }
+        else{
+            throw new JogadorException(DeleteError, null);
         }
     }
 
     @Override
-    public void update(Clube c,Jogador Jogadorvelho, Jogador Jogadornovo) {
+    public void update(Clube c,Jogador Jogadorvelho, Jogador Jogadornovo) throws JogadorException{
         if (lista.containsKey(c.getNome())){
             if(lista.values().contains(Jogadorvelho)){
                 lista.values().remove(Jogadorvelho);
                 jogadores.add(Jogadornovo);
                 lista.put(c.getNome(),jogadores);
             }
+            else{
+                throw new JogadorException(UpdateError,null);
+            }
+        }
+        else{
+            throw new JogadorException(UpdateError,null);
         }
     }
 
     @Override
-    public Jogador find(Clube c,Jogador a) {
+    public Jogador find(Clube c,Jogador a) throws JogadorException{
         if(lista.containsKey(c.getNome())){
             if(lista.values().contains(a)){
                 return a;
             }
             else{
-                return null;
+                throw new JogadorException(FindError,null);
             }
         }
         else{
-            return null;
+            throw new JogadorException(FindError,null);
         }
     }
 }
