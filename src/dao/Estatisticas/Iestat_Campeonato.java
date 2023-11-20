19f3fdc732621827082ed3743404a610c4aeeb30
Armando <111616936+Armaaaaaaaaaaaaaaaaaaaaaaaaaaando@users.noreply.github.com>
2023-11-20 10:31:07 -0300

    Atualização: Criação das exceções de Campeonatodao e interface campeonato dao

--- src/dao/Estatisticas/Iestat_Campeonato.java
+++ src/dao/Estatisticas/Iestat_Campeonato.java
@@ -1,15 +1,16 @@
 package src.dao.Estatisticas;
 
+import src.Excecoes.Estatisticas.CampeonatoException;
 import src.model.Estatisticas.Estat_Campeonato;
 import src.model.Jogo.Clube;
 
 import java.util.LinkedList;
 
 public interface Iestat_Campeonato {
-    public void save(Estat_Campeonato a);
-    public void delete(Estat_Campeonato a);
-    public void update(Estat_Campeonato clubevelho, Estat_Campeonato clubenovo);
-    public Estat_Campeonato find(Estat_Campeonato a);
+    public void save(Estat_Campeonato a) throws CampeonatoException;
+    public void delete(Estat_Campeonato a)throws CampeonatoException;
+    public void update(Estat_Campeonato clubevelho, Estat_Campeonato clubenovo)throws CampeonatoException;
+    public Estat_Campeonato find(Estat_Campeonato a)throws CampeonatoException;
 
 
 }
--- src/dao/Estatisticas/estat_CampeonatoDao.java
+++ src/dao/Estatisticas/estat_CampeonatoDao.java
@@ -1,41 +1,53 @@
 package src.dao.Estatisticas;
 
+import src.Excecoes.Estatisticas.CampeonatoException;
 import src.model.Estatisticas.Estat_Campeonato;
 import src.model.Jogo.Clube;
 
 import java.util.LinkedList;
 
+import static src.util.Constantes.*;
+
 public class estat_CampeonatoDao implements Iestat_Campeonato{
     private LinkedList<Estat_Campeonato> lista = new LinkedList<>();
     @Override
-    public void save(Estat_Campeonato a) {
+    public void save(Estat_Campeonato a) throws CampeonatoException {
         if(!lista.contains(a)){
             lista.add(a);
         }
-    }
-
-    @Override
-    public void delete(Estat_Campeonato a) {
-        if(lista.contains(a)){
-            lista.remove(a);
+        else{
+            throw new CampeonatoException(SaveError,null);
         }
     }
 
     @Override
-    public void update(Estat_Campeonato clubevelho, Estat_Campeonato clubenovo) {
+    public void delete(Estat_Campeonato a) throws CampeonatoException{
+        if(lista.contains(a)){
+            lista.remove(a);
+        }
+        else{
+            throw new CampeonatoException(DeleteError,null);
+        }
+    }
+
+    @Override
+    public void update(Estat_Campeonato clubevelho, Estat_Campeonato clubenovo) throws CampeonatoException{
         if(lista.contains(clubevelho)){
             lista.remove(clubenovo);
             lista.add(clubenovo);
         }
+        else{
+            throw new CampeonatoException(UpdateError,null);
+        }
     }
 
     @Override
-    public Estat_Campeonato find(Estat_Campeonato a) {
+    public Estat_Campeonato find(Estat_Campeonato a) throws CampeonatoException{
         if(lista.contains(a)){
             return a;
         }
         else {
-            return null;
+            throw new CampeonatoException(FindError,null);
         }
     }
 }
