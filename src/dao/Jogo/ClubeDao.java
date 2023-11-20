c4116b6bce86aa39e3238a5684ab101923564579
Armando <111616936+Armaaaaaaaaaaaaaaaaaaaaaaaaaaando@users.noreply.github.com>
2023-11-20 10:27:16 -0300

    Atualização: Criação das exceções de Clubedao, e interface ClubeDao

--- src/dao/Jogo/ClubeDao.java
+++ src/dao/Jogo/ClubeDao.java
@@ -1,41 +1,54 @@
 package src.dao.Jogo;
 
+import src.Excecoes.Jogo.ClubeException;
 import src.model.Jogo.Clube;
 
 import java.util.LinkedList;
+
+import static src.util.Constantes.*;
 
 public class ClubeDao implements IClubeDao{
     private LinkedList<Clube> lista = new LinkedList<>();
 
     @Override
-    public void save(Clube a) {
+    public void save(Clube a) throws ClubeException {
         if(!lista.contains(a)){
             lista.add(a);
         }
+        else{
+            throw new ClubeException(SaveError, null);
+        }
+
     }
 
     @Override
-    public void delete(Clube a) {
+    public void delete(Clube a) throws ClubeException{
         if(lista.contains(a)){
             lista.remove(a);
+        }
+        else{
+            throw new ClubeException(DeleteError,null);
         }
     }
 
     @Override
-    public void update(Clube clubevelho, Clube clubenovo) {
+    public void update(Clube clubevelho, Clube clubenovo) throws ClubeException {
         if(lista.contains(clubevelho)){
             lista.remove(clubenovo);
             lista.add(clubenovo);
         }
+        else{
+            throw new ClubeException(UpdateError,null);
+        }
     }
 
     @Override
-    public Clube find(Clube a) {
+    public Clube find(Clube a) throws ClubeException{
         if(lista.contains(a)){
             return a;
         }
         else {
-            return null;
+            throw new ClubeException(FindError,null);
         }
     }
     @Override
--- src/dao/Jogo/IClubeDao.java
+++ src/dao/Jogo/IClubeDao.java
@@ -1,16 +1,17 @@
 package src.dao.Jogo;
 
 
+import src.Excecoes.Jogo.ClubeException;
 import src.model.Jogo.Clube;
 
 import java.util.LinkedList;
 
 public interface IClubeDao {
-    public void save(Clube a);
-    public void delete(Clube a);
-    public void update(Clube clubevelho, Clube clubenovo);
-    public Clube find(Clube a);
-    public int findSize();
-    public LinkedList findAll();
+    public void save(Clube a) throws ClubeException;
+    public void delete(Clube a)throws ClubeException;
+    public void update(Clube clubevelho, Clube clubenovo)throws ClubeException;
+    public Clube find(Clube a)throws ClubeException;
+    public int findSize()throws ClubeException;
+    public LinkedList findAll()throws ClubeException;
 }
 
